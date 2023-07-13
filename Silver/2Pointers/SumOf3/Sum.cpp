fe43a1494e5cc911d090af462878313af3fbf2b4
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-07-13 14:49:28 -0400

    Update Sum.cpp

--- Silver/2Pointers/SumOf3/Sum.cpp
+++ Silver/2Pointers/SumOf3/Sum.cpp
@@ -13,5 +13,24 @@ int main(){
     cin.tie(0)->sync_with_stdio(0);
     freopen("","r",stdin);
     int n,x; cin >> n >> x;
+    vector<pair<int,int>> arr(n);
+    for(int i = 0; i < n;i++){
+        cin >> arr[i].first;
+        arr[i].second = i;
+    }
+    sort(arr,arr+n);
+    for(int i = 1; i < n-1;i++){
+        int left = 0, right = n-1;
+        while(left != right){
+            if(arr[left].first + arr[i].first + arr[right].first > x){
+                right--;
+            }else if(arr[left].first + arr[i].first + arr[right].first < x){
+                left++;
+            }else if(arr[left].first + arr[i].first + arr[right].first == x && left != i && right != i){
+                cout << arr[left].second << " " << arr[i]
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << endl;
     return 0;
 }
