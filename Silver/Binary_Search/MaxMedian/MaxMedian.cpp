027c4a031610c4da68b2134d5e01fbcc95062f62
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-07-20 13:08:49 -0400

    dfg

---
+++ Silver/Binary_Search/MaxMedian/MaxMedian.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define INF 1e18
+#define MOD 1e9+7
+#define all(x) begin(x),end(x)
+#define rall(x) x.rbegin(), x.rend()
+
+typedef long long ll;
+
+using namespace std; 
+
+const int dirX[] = {-1, 0, 1, 0};
+const int dirY[] = {0, 1, 0, -1};
+
+int LOG2(int n){
+    int ret = 0; 
+    while((1<<(ret+1)) <= n){ret++;}
+    return ret;
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("","r",stdin);
+    return 0;
+}
--- Silver/Greedy/HelpCross/helpcrossBetter.cpp
+++ Silver/Greedy/HelpCross/helpcrossBetter.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define INF 1e9
-
+#define all(x) begin(x),end(x)
 
 using namespace std; 
 
@@ -17,5 +17,22 @@ int main(){
     for(int i = 0; i < n;i++){
         cin >> cows[i].first >> cows[i].second;
     }
+    sort(all(cows));
+    int ans = 0, cowIdx = 0;
+    priority_queue<int> availableCows;
+    for(int c : chickens){
+        while(cowIdx < n && cows[cowIdx] <= c){
+            availableCows.push(-cows[cowIdx].second);
+            cowIdx++;
+        }
+        while(!availableCows.empty && -availableCows.top < c){
+            availableCows.pop();
+        }
+        if(!availableCows.empty){
+            ans++;
+            availableCows.pop();
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
