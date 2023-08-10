2fe33fe66cef4e13ffa4a0341e05938c12725ca7
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-08-10 17:11:29 -0400

    1 problem

--- Codeforces/Fibonaccarsis/fibonaccharsis.cpp
+++ Codeforces/Fibonaccarsis/fibonaccharsis.cpp
@@ -8,25 +8,40 @@ typedef long long ll;
 
 using namespace std;
 
-int LOG2(int n){
-    int ret = 0; 
-    while((1<<(ret+1)) <= n){ret++;}
-    return ret;
-}
+/*
+instead of looking for the first 2 numbers that can lead to a sequence, look for the number before n
+
+then simulate downwards for k steps to see if its a valid sequence
+
+WHat I learned:
+- Try looking at solving brute force in reverse for a faster solution
+*/
 
 
 void solve(){
-    int n; ll k; cin >> n >> k;
+    int n; int k; cin >> n >> k;
     int ans = 0;
     for(int i = 1; i <= n;i++){
-                 
+        int second = n;
+        int first = i;
+        bool valid = true;
+        for(int j = 0; j < k-2;j++){
+            int fx = first;
+            first = second - fx;
+            second = fx;
+            valid &= first <= second;
+            valid &=  min(first,second) >= 0;
+            if(!valid) break;
+        }
+        if(valid) ans++;
     }
+    cout << ans << endl;
     
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    freopen("","r",stdin);
+    // freopen("fib.in","r",stdin);
     int t; cin >> t;
     while(t--){solve();}
     return 0;
