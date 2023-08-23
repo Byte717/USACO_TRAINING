9d626e0e29a00dee02a3d292dc8f7d441208df63
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-08-23 14:58:01 -0400

    1 problem

---
+++ Codeforces/SerejaBottles/serejaBottles.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define INF 1e18
+#define MOD 1e9+7
+#define all(x) begin(x),end(x)
+#define rall(x) x.rbegin(), x.rend()
+
+typedef long long ll;
+
+using namespace std;
+
+const int dirX[] = {-1, 0, 1, 0};
+const int dirY[] = {0, 1, 0, -1};
+
+int LOG2(int n){
+    int ret = 0; 
+    while((1<<(ret+1)) <= n){ret++;}
+    return ret;
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    // freopen("","r",stdin);
+    int n; cin >> n;
+    vector<pair<int,int>> bottles(n);
+    for(auto &[x,y] : bottles){
+        cin >> x >> y;
+    }
+    int ans = 0;
+    for(int i = 0; i < n;i++){
+        bool foundOpener = false;
+        for(int j = 0; j < n;j++){
+            if(i != j){
+                if(bottles[j].second == bottles[i].first){
+                    foundOpener = true;
+                }
+            }
+        }
+        if(!foundOpener){
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
