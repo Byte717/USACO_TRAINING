cd079fe4ef34562bada22fd23c76a8d1029d6e22
Byte717 <dhairyasarvaiya455@gmail.com>
2023-08-28 08:29:25 -0400

    test on termux

--- test.cpp
+++ test.cpp
@@ -1,53 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 
-vector<vector<int>> adj;
-
-
-vector<bool> visited;
-
-void dfs(int node){
-	if(visited[node]){
-		return;
-	}
-	visited[node] = true;
-	for(int &i : adj[node]){
-		dfs(i);
-	}
-}
-
-void bfs(int start){
-	queue<int> q;
-	q.push(start);
-	while(!q.empty()){
-
-		int top = q.front(); q.pop();
-
-		if(!visited[top]){
-
-			visited[top] = true;
-
-			for(int &i : adj[top]){
-				q.push(i);
-			}
-			
-		}
-	}
-}
-
-
-int main() {
-	int n; cin >> n;
-	adj.resize(n);
-	for(int i = 0; i < n;i++){
-		int a, b; cin >> a >> b;
-		a--; b--;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-	 
-	return 0;
+int main(){
+    cout << "HELLO WORLD";
+    return 0;
 }
 
