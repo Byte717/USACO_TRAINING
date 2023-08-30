cd4749283eaa0e14d8716720edacd199e1652f02
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-08-30 07:32:23 -0400

    1 problem

--- Codeforces/PowerOfPoints/Pop.cpp
+++ Codeforces/PowerOfPoints/Pop.cpp
@@ -9,22 +9,40 @@ using namespace std;
 const ll INF = 1e18;
 const ll MOD = 1e9+7;
 
-const int dirX[4] = {-1, 0, 1, 0};
-const int dirY[4] = {0, 1, 0, -1};
+/*
+solution: so each interval contributed b-a+1. So based on that, sort the points, then for each ith point as S, you need the sum of the past and future points
+the formula is n+x[i]*(2*i-n) - sums before + sums after. 
 
-int LOG2(int n){
-    int ret = 0; 
-    while((1<<(ret+1)) <= n){ret++;}
-    return ret;
-}
+What I learned Power of Points:
+    - Prefix sums imply summation and vice versa.
+    - intervals can be counted as their length
+    - sort things, makes it easier. 
+
+*/
+
 void solve(){
-    int n; cin >> n;
-    
+    ll n; cin >> n;
+    vector<pair<ll,ll>> x(n+1);
+    ll s1 = 0, s2 = 0;
+    for(ll i = 1; i <= n; i++){
+        cin >> x[i].first;
+        x[i].second = i;
+        s2 += x[i].first;
+    }
+    vector<ll> ans(n+1);
+    sort(x.begin()+1, x.end());
+    for(ll i = 1; i <= n;i++){
+        s2 -= x[i].first;
+        s1 += x[i].first;
+        ans[x[i].second] = n+x[i].first*(2*i-n)-s1+s2;
+    }
+    for(ll i = 1; i <= n;i++) cout << ans[i] << ' ';
+    cout << endl;
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    freopen("","r",stdin);
+    // freopen("pop.in","r",stdin);
     int t; cin >> t;
     while(t--){
         solve();
