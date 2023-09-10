8020fe2edb53b76994c87aeee18d7f8e7e8d827f
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-09-10 09:43:55 -0400

    1 problem

--- Silver/Flood Fill/whyCross3/whyCross3.cpp
+++ Silver/Flood Fill/whyCross3/whyCross3.cpp
@@ -2,23 +2,25 @@
 #include <cstdio>
 #include <algorithm>
 #include <vector>
+#include <set>
+#include <functional>
 
 using namespace std;
 
 
-vector<vector<bool> vis;
+vector<vector<bool>> vis;
 
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    freopen("whyCross3.in","r",stdin);
-    int n, k r; cin >> n >> k >> r;
+    freopen("countcross.in","r",stdin);
+    int n, k, r; cin >> n >> k >> r;
     vis = vector<vector<bool>>(n,vector<bool>(n,false));
-    set<pair<int,int>> roads;
+    vector<vector<set<pair<int,int>>>> roads(n, vector<set<pair<int,int>>>(n));
     for(int i = 0; i < r;i++){
         int a, b, c, d; cin >> a >> b >> c >> d;
-        roads.insert({--a,--b});
-        roads.insert({--c,--d});
+        roads[--a][--b].insert({--c,--d});
+        roads[c][d].insert({a,b});
     }
     vector<pair<int,int>> cows(k);
     for(int i = 0; i < k;i++){
@@ -27,30 +29,30 @@ int main(){
     }
     const int dX[4] = {0,1,0,-1};
     const int dY[4] = {1,0,-1,0};
-    auto floodFill = [&](int x, int y, bool start){
-        if(x < 0 || x >= n || y >=n || y < 0) return;
+    std::function<void(int,int)> floodFill = [&](int x, int y){
+        if(x < 0 || x >= n || y >= n || y < 0) return;
         
         if(vis[x][y]) return;
-        if(!start && roads.find({x,y}) != roads.end()){
-            return;
-        }
         vis[x][y] = true;
         for(int i = 0; i < 4;i++){
-            floodFill(x+dX[i],y+dY[i],false);
+            int newX = x + dX[i], newY = y + dY[i];
+            if(roads[x][y].count({newX,newY}) == 0){
+                floodFill(newX,newY);
+            }
         }
 
     };
-
+    // floodFill(1,1,true);
     int ans = 0;
     for(int i = 0; i < k-1;i++){
+        vis = vector<vector<bool>>(n,vector<bool>(n,false));
+        floodFill(cows[i].first,cows[i].second);
         for(int j = i+1; j < k;j++){
-            vis = vector<vector<bool>>(n,vector<bool>(n,false));
-            floodFill(cows[i].first,cows[i].second,true);
-            if(vis[cows[i].first][cows[i].second] && vis[cows[j].first][cows[j].second]){
+            if(!vis[cows[j].first][cows[j].second]){
                 ans++;
             }
-
         }
     }
+    freopen("countcross.out","w",stdout);
     cout << ans << endl;
 }
