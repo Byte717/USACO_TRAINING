2b42604e76844fb8798b432a678a4494f8054d62
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-09-30 16:05:10 -0400

    1 problem

---
+++ Silver/Binary_Search/Loan_Repayment/loan2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+typedef long long ll;
+
+using namespace std;
+
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("loan.in","r",stdin);
+    freopen("loan.out","w",stdout);
+    ll n, m, k;
+    cin >> n >> k >> m;
+
+    auto works = [&](ll x){
+        ll leftOver = n;
+        ll daysPassed = 0;
+        while(daysPassed < k){
+            ll amount = leftOver/x;
+            if(amount < m){
+                ll daysNeeded = leftOver/m;
+                if(leftOver % m != 0) daysNeeded++;
+                leftOver -= (daysNeeded*m);
+                daysPassed+= daysNeeded;
+                break;
+            }else{
+                ll distFromMult = leftOver - (x*(leftOver/x));
+                if(distFromMult == 0){
+                    daysPassed++;
+                    leftOver -= (leftOver)/x;
+                    continue;
+                }
+                ll daysNeeded = distFromMult/amount;
+                if(distFromMult % amount != 0) daysNeeded++;
+                leftOver -= amount*daysNeeded;
+                daysPassed += daysNeeded;
+            }
+        }
+        return daysPassed <= k && leftOver <= 0; 
+    };
+    // cout << works(4826) << endl;
+    ll low = 1, high = 1e18, ans = -1;
+    while(low <= high){
+        ll mid = low + (high-low)/2;
+        if(works(mid)){
+            ans = mid;
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
