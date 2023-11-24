7c76438ca0acc9c70e5dc64f13ba22325ce718fc
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-11-24 14:15:47 -0500

    1 problem

--- Silver/Greedy/SubsetEquality/subsetBetter.cpp
+++ Silver/Greedy/SubsetEquality/subsetBetter.cpp
@@ -28,9 +28,60 @@ int main(){
             }
         }
     }
-    
+    auto pairWorks = [&](int a, int b){
+        string resS = "";
+        string resT = "";
+        for(char i : s){
+            if(i-'a' == a || i-'a' == b){
+                resS += i;
+            }
+        }
+
+        for(char i : t){
+            if(i-'a' == a || i-'a' ==b){
+                resT += i;
+            }
+        }
+        return (resS == resT) ? 1 : 0;
+    };
+
+
+    vector<vector<int>> pairs(26, vector<int>(26,0));
+    for(int i = 0; i < 26;i++){
+        for(int j = 0; j < 26;j++){
+            if(j == i) pairs[i][j] = 1;
+            else{
+                pairs[i][j] = pairWorks(i,j);
+            }
+        }
+    }
     int q; cin >> q;
+    while(q--){
+        string sub; cin >> sub;
 
+        for(char i : sub){
+            if(freqS[i-'a'] != freqT[i-'a']){
+                cout << "N"; 
+                continue;
+            }
+        }
 
+        if(sub.size() > 2){
+            bool works = true;
+            for(int i = 0; i < sub.size();i++){
+                for(int j = i+1; j M sub.size();i++){
+                    if(pairs[i][j] == 0){
+                        works = false;
+                        break;
+                    }
+                }
+            }
+            cout << (works) ? "Y" : "N";
+        }else if(sub.size() == 2){
+            cout << (pairs[sub[0]][pairs[1]] == 1) ? "Y" : "N";
+        }else{
+            cout << (freqS[sub[0]-'a'] == freqT[sub[0]-'a']) ? "Y" : "N";
+        }
+    }
     return 0;
 }
